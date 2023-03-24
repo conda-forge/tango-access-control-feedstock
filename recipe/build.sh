7da4e553cf8eca86bbe79275f73e3b9bebbe20da
Benjamin Bertrand <benjamin.bertrand@maxiv.lu.se>
2023-03-24 11:57:09 +0100

    Clean build script

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,7 +3,6 @@ cd TangoAccessControl/build
 cmake ${CMAKE_ARGS} \
       -DCMAKE_BUILD_TYPE=Release \
       -DCMAKE_VERBOSE_MAKEFILE=true \
-      -DMYSQL_INCLUDE_DIR=$PREFIX/include/mysql \
       ..
 
 make -j $CPU_COUNT
